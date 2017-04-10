e0d79f9cf590ca9def3e037a83099df49c811a2a
John Lima <john.lima_desenv@outlook.com>
2017-04-10 16:44:39 -0300

    # Implementação do fastlane;

--- WeatherMapUITests/WeatherMapUITests.swift
+++ WeatherMapUITests/WeatherMapUITests.swift
@@ -20,6 +20,7 @@ class WeatherMapUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
@@ -34,16 +35,20 @@ class WeatherMapUITests: XCTestCase {
     
         let app = XCUIApplication()
         let navigationBar = app.navigationBars.element(boundBy: 0)
+        let alert = app.alerts.element(boundBy: 0)
+        
+        if alert.exists {
+            alert.buttons.element(boundBy: 1).tap()
+        }
         
         navigationBar.buttons.element(boundBy: 0).tap()
-        
         navigationBar.buttons.element(boundBy: 1).tap()
         sleep(5)
-        // Map
+        snapshot("Map")
         
         navigationBar.buttons.element(boundBy: 0).tap()
         navigationBar.buttons.element(boundBy: 1).tap()
         sleep(5)
-        // List
+        snapshot("List")
     }
 }
